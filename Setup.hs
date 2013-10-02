2074e3e20d6aebbbf75e8e0945a243af179fabcd
Ian Duncan <ian.duncan@logos.com>
2013-10-01 21:22:33 -0700

    Dotfiling.

---
+++ Setup.hs
@@ -0,0 +1,56 @@
+module Setup where
+import Control.Monad
+import System.Directory
+import System.FilePath
+import System.Posix.Files
+
+main = do
+  linkDotfiles
+  -- linkBinFolder
+
+linkBinFolder :: IO ()
+linkBinFolder = do
+  bin <- canonicalizePath "./bin"
+  home <- getHomeDirectory
+  linked <- linkNX bin (home </> "bin")
+  if linked
+    then putStrLn "Linking ./bin to ~/bin"
+    else return ()
+
+-- link if not already linked. returns bool for success
+linkNX :: FilePath -> FilePath -> IO Bool
+linkNX source destination = do
+  fileExists <- doesFileExist destination
+  if fileExists
+    then return False
+    else do
+      createSymbolicLink source destination
+      return True
+
+linkDotfiles :: IO ()
+linkDotfiles = do
+  dotfiles <- getDirectoryContents "./dotfiles"
+  home <- getHomeDirectory
+  let contents = filter (\x -> x /= "." && x /= "..") dotfiles
+  mapM_ (linkDotfile home) contents
+
+linkDotfile :: FilePath -> FilePath -> IO ()
+linkDotfile home dotfile = do
+  let homeLocation = home </> ('.' : dotfile)
+      realLocation = "./dotfiles" </> dotfile
+  putStr ("Linking " ++ realLocation ++ " to " ++ takeFileName homeLocation ++ "... ")
+  symlinkExists <- do
+    fileExists <- doesFileExist homeLocation
+    if fileExists
+      then do
+        status <- getSymbolicLinkStatus homeLocation
+        if isSymbolicLink status
+          then putStr "already exists " >> return True
+          else removeFile homeLocation >> return False
+      else return False
+  if symlinkExists
+    then putStrLn "\ESC[31m✗\ESC[0m"
+    else do
+      canonicalDotfilePath <- canonicalizePath realLocation
+      createSymbolicLink canonicalDotfilePath homeLocation
+      putStrLn "\ESC[32m✓\ESC[0m"
